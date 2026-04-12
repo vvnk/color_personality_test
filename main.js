27b76f977c058bd0d3637be1b5017a69181781b8
Vivienne K <kamvivienne@gmail.com>
2026-04-12 23:44:38 +0000

    fixed the color tracking function to track if user made changes to their answer

--- main.js
+++ main.js
@@ -1,20 +1,17 @@
 // Initializing counters for each color (Red, Yellow, Green, Blue)
-let R = 0;
-let Y = 0;
-let G = 0;
-let B = 0;
+let answers = {q1: null, q2: null, q3: null, q4: null, q5: null, q6: null, q7: null, q8: null, q9: null, q10: null, q11: null, q12: null, q13: null};
 
-function counter(color) {
-    if (color === 'R') {
-        R++;
-    } else if (color === 'Y') {
-        Y++;
-    } else if (color === 'G') {
-        G++;
-    } else if (color === 'B') {
-        B++;
+function counter(question, choice) {
+    answers[question] = choice;
+    let R = 0, Y = 0, G = 0, B = 0;
+
+    for (let color in answers) {
+        if (answers[color] === 'R') R++;
+        else if (answers[color] === 'Y') Y++;
+        else if (answers[color] === 'G') G++;
+        else if (answers[color] === 'B') B++;
     }
-    
+    console.log("function ran");
     document.getElementById("output").innerText =
-    "R: " + R + " | Y: " + Y + " | G: " + G + " | B: " + B;
+    "R: " + R + " Y: " + Y + " G: " + G + " B: " + B;
 }
