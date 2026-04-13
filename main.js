80377ff8d63b7ecd7e39a5ee5bcdbfe81087ca80
Vivienne K <kamvivienne@gmail.com>
2026-04-13 00:00:05 +0000

    results page! but the results don't work?

--- main.js
+++ main.js
@@ -1,6 +1,7 @@
 // Initializing counters for each color (Red, Yellow, Green, Blue)
 let answers = {q1: null, q2: null, q3: null, q4: null, q5: null, q6: null, q7: null, q8: null, q9: null, q10: null, q11: null, q12: null, q13: null};
 
+// Track the user's answers and update the counters
 function counter(question, choice) {
     answers[question] = choice;
     let R = 0, Y = 0, G = 0, B = 0;
@@ -15,3 +16,22 @@ function counter(question, choice) {
     document.getElementById("output").innerText =
     "R: " + R + " Y: " + Y + " G: " + G + " B: " + B;
 }
+
+// Determine the user's results based on their highest color choices
+let top = Math.max(R, Y, G, B);
+let result = "";
+let desc = "";
+
+if (top === R) {
+    result = "You are mostly Red!";
+    desc = "You are a natural leader, confident, and driven. You thrive in competitive environments and are not afraid to take risks. You can be assertive and direct, but also have a strong sense of loyalty to those you care about.";
+} else if (top === Y) {
+    result = "You are mostly Yellow!";
+    desc = "You are a very happy person!";
+} else if (top === G) {
+    result = "You are mostly Green!";
+    desc = "You are a green green bean!";
+} else if (top === B) {
+    result = "You are mostly Blue!";
+    desc = "Wow you are so blue";
+} 
